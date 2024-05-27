7f5cf14957ca8fc00058a300785aea853eadf0f6
Ana Gotardi <gotardiana@gmail.com>
2024-05-27 11:17:33 -0300

    27/5

--- Assets/Scripts/obstaculo.cs
+++ Assets/Scripts/obstaculo.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class obstaculo : MonoBehaviour
 {
+    [SerializeField]
     private float velocidade = 0.6f;
 
+    [SerializeField]
+    private float variacaoposicaoY;
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoposicaoY,variacaoposicaoY));
+    }
 
     // Update is called once per frame
     void Update()
